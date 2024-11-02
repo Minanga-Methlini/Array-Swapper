e2cbed2dc4fb56667c509582190bba48e9e179d7
Minanga Methlini <minangamethliniiddamalgoda@gmail.com>
2024-11-02 07:18:08 +0530

    Update ArraySwapperr.java

--- ArraySwapperr.java
+++ ArraySwapperr.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package arrayswapperr;
 import java.util.Arrays;
-/**
- *
- * @author Minanga Methlini
- */
+
 public class ArraySwapperr {
 
     static Integer[] array = {76,6,107,92,21,23,5,9,8,8143};
@@ -44,11 +37,9 @@ public class ArraySwapperr {
             insert(outerIndex);
         }
     }
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         
         System.out.println("Array befpre sorting:" + Arrays.deepToString(array));
         
